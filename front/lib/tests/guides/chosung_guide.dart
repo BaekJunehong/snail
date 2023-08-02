a9520d41d8586cde7c9707a9b03674986785b4a3
minjeong-kim-git <fran0666@naver.com>
2023-08-02 14:53:02 +0900

    검사 가이드 및 에셋 정리

---
+++ front/lib/tests/guides/chosung_guide.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ChosungGuideScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/pattern.png', fit: BoxFit.fill),
+          ),
+          Center(
+            child: Container(
+              //모달
+              width: 862,
+              height: 554,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '단어 유창성',
+                      style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '소요 시간: 03분',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      '화면에서 보여주는 자음으로 시작하는 단어를 최대한 많이 말해주세요!',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Image.asset(
+                      'assets/chosung.png',
+                      width: 478,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ front/lib/tests/guides/line_guide.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LineGuideScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/pattern.png', fit: BoxFit.fill),
+          ),
+          Center(
+            child: Container(
+              //모달
+              width: 862,
+              height: 554,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '선로 잇기',
+                      style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '소요 시간: 03분',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      '1부터 순서대로 점을 이어 그림을 완성해주세요!',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Image.asset(
+                      'assets/line.png',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
