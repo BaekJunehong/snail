c8c63f60502cc532f15c9f114d07c511476a3c16
Heechan Kim <caphile98@gmail.com>
2023-08-06 22:12:54 +0900

    ec2 server / index.js

---
+++ backend/index.js
@@ -0,0 +1,64 @@
+// ftp로 ec2 서버에 현재 index.js를 옮긴 후
+// putty를 사용해 node index.js 실행 후 사용가능
+
+const express    = require('express'); //express import
+const mysql      = require('mysql'); // mysql import
+const dbconfig   = require('./config/database.js'); //master 연결 정보
+//const dbconfig_slave = require('./config/database-slave.js'); // slave 연결 정보
+const connection = mysql.createConnection(dbconfig); // master 연결
+//const connection_slave = mysql.createConnection(dbconfig_slave); // slave 연결
+const bodyParser = require('body-parser') // json 형식으로 받기 위한 body-parser 모듈
+const app = express();
+
+app.set('port', process.env.PORT || 3000); // 포트
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const cors = require('cors');
+app.use(cors());  // 모든 origin 요청 허용
+
+//-------------------------------------------------------------------------------------
+// Query
+
+// 회원정보 저장(회원가입)
+app.post('/saveUserInfo', (req, res) => {
+  const user_id = req.body.USER_ID;
+  const user_pw = req.body.USER_PW;
+
+  const query = 'INSERT INTO PARENT (USER_ID, USER_PW) VALUES (?, ?)';
+
+  connection.query(query, [user_id, user_pw], (err, result) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const id = result.insertId;
+
+    res.set('Location', `/user/${id}`);
+
+    res.status(200).send({id});
+  });
+});
+
+// 아이디 중복 확인
+app.get('/checkDuplicatedID', (req, res) => {
+  const user_id = req.query.USER_ID;
+
+  const query = 'SELECT EXISTS(SELECT 1 FROM PARENT WHERE USER_ID = ?) as exist';
+
+  connection.query(query, [user_id], (err, rows) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const exist = rows[0].exist;
+
+    res.status(200).send({exist});
+  });
+});
+
+//-------------------------------------------------------------------------------------
+// listener
+app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${app.get('port')}`);
+});
