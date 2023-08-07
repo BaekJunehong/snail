9b77b5ccb74b69c699c362fc3fbb637fbbb52733
minjeong-kim-git <fran0666@naver.com>
2023-08-07 15:59:36 +0900

    결과 페이지 차트 렌더링

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:snail/login.dart';
-import 'package:snail/tests/result/loadingresult.dart';
+import 'package:snail/tests/result/dashboard/parentnote.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       ),
       title: 'SNaiL',
       home: Scaffold(
-        body: LoadingResultScreen(),
+        body: ParentNoteScreen(),
       ), //처음 접하는 화면을 SplashScreen으로 설정.
     );
   }
