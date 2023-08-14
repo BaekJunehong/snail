a14558053cc1ed82e8e64802129b86c02a6c4d68
minjeong-kim-git <fran0666@naver.com>
2023-08-14 13:42:35 +0900

    이야기 검사 및 뒤로가기 UI

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:snail/facerecognition.dart';
+import 'package:snail/splash.dart';
+import 'package:snail/tests/guides/backspace_guide.dart';
+import 'package:snail/tests/tests/story_video.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
       ),
       title: 'SNaiL',
       home: Scaffold(
-        body: FaceRecognitionScreen(),
+        body: StoryVideoScreen(),
       ), //처음 접하는 화면을 SplashScreen으로 설정.
     );
   }
