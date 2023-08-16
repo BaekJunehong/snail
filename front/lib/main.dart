a79a3effabea7b7ca20199d42f81cf0c92a60089
miffDONG <ksglsg1350@gmail.com>
2023-08-16 17:20:39 +0900

    story_test video play finish

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:snail/splash.dart';
-import 'package:snail/test/chosung_test.dart';
-import 'package:snail/tests/result/dashboard/parentnote.dart';
-import 'package:snail/test/stroop_test.dart';
-import 'package:snail/test/line_test.dart';
-import 'package:snail/test/follow_the_words.dart';
-=======
 import 'package:snail/tests/result/noresults.dart';
->>>>>>> da49a3e65ea181f2b902837dc262379687cfaf45
+import 'package:snail/tests/test/line_test.dart';
+import 'package:snail/tests/test/story_test.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,11 +17,7 @@ class MyApp extends StatelessWidget {
       ),
       title: 'SNaiL',
       home: Scaffold(
-<<<<<<< HEAD
-        body: followTest(),
-=======
-        body: NoResultScreen(),
->>>>>>> da49a3e65ea181f2b902837dc262379687cfaf45
+        body: StoryTest(),
       ), //처음 접하는 화면을 SplashScreen으로 설정.
     );
   }
--- front/lib/tests/test/story_test.dart
+++ front/lib/tests/test/story_test.dart
@@ -1,14 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:snail/splash.dart';
 import 'package:universal_html/html.dart' as html;
 import 'package:flutter/foundation.dart';
 
-class VideoApp extends StatefulWidget {
+class StoryTest extends StatefulWidget {
   @override
-  _VideoAppState createState() => _VideoAppState();
+  _StoryTestState createState() => _StoryTestState();
 }
 
-class _VideoAppState extends State<VideoApp> {
+class _StoryTestState extends State<StoryTest> {
+  //영상은 url로 가져오고 영상 관련 이미지는 asset에 저장.
+  //list = [url,이미지,[문제,답]]로 생성으로 할 듯 합니다.
+
   String videoUrl =
       'https://www.nlcy.go.kr/multiLanguageStory/2022/Nlcy_001_003/Nlcy_001_003_ko.mp4'; // 예시 URL
   bool _isVideoCompleted = false;
@@ -16,7 +21,15 @@ class _VideoAppState extends State<VideoApp> {
   @override
   void initState() {
     super.initState();
+    _isVideoCompleted = false;
     playVideo(videoUrl);
+
+    //04분 후 활성화
+    Timer(Duration(minutes: 4), () {
+      setState(() {
+        _isVideoCompleted = true;
+      });
+    });
   }
 
   void playVideo(String videoUrl) {
@@ -30,11 +43,6 @@ class _VideoAppState extends State<VideoApp> {
         if (a != null) {
           a.dispatchEvent(html.MouseEvent('click'));
         }
-        v.addEventListener('ended', (_) {
-          setState(() {
-            _isVideoCompleted = true;
-          });
-        });
       }
     }
   }
@@ -59,33 +67,32 @@ class _VideoAppState extends State<VideoApp> {
             Container(
               width: 872,
               height: 539,
-              color: Colors.black,
-              child: ElevatedButton(
-                onPressed: _isVideoCompleted
-                    ? () {
-                        Navigator.push(
+              child: Image.asset(
+                'story_sample.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+            SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: _isVideoCompleted
+                  ? () {
+                      Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => SplashScreen(),
-                          ),
-                        );
-                      }
-                    : null,
-                child: Text(
-                  '시작하기',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary:
-                      _isVideoCompleted ? Color(0XFFffcb39) : Color(0XFFd9d9d9),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  fixedSize: Size(165, 48),
-                ),
+                              builder: (context) => (SplashScreen())));
+                    }
+                  : null,
+              child: Text(
+                '시작하기',
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary:
+                    _isVideoCompleted ? Color(0XFFffcb39) : Color(0XFFd9d9d9),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24)),
+                fixedSize: Size(165, 48),
               ),
             ),
           ],
